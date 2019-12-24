23b0c09096d6af79e78c1170ba950543b4aab2cc
himanshu-orsolutions <himanshu.optiroutesolutions@gmail.com>
2019-12-24 17:28:50 +0530

    intial commit

---
+++ backend/class-manager/src/test/java/com/che/classmanager/ClassManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.che.classmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
