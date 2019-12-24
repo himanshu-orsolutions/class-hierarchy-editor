dae299d58b1641e0c0e440a161de1a090fc5588d
himanshu-orsolutions <himanshu.optiroutesolutions@gmail.com>
2019-12-24 17:48:01 +0530

    add models

--- backend/class-manager/src/main/java/com/che/classmanager/ClassManagerApplication.java
+++ backend/class-manager/src/main/java/com/che/classmanager/ClassManagerApplication.java
@@ -9,5 +9,4 @@ public class ClassManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClassManagerApplication.class, args);
 	}
-
 }
---
+++ backend/class-manager/src/main/java/com/che/classmanager/models/Class.java
@@ -0,0 +1,44 @@
+package com.che.classmanager.models;
+
+/**
+ * The model Class. It holds information of a class.
+ */
+public class Class {
+
+	private String cid;
+	private String name;
+	private String isAbstract;
+	private String pid;
+
+	public String getCid() {
+		return cid;
+	}
+
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIsAbstract() {
+		return isAbstract;
+	}
+
+	public void setIsAbstract(String isAbstract) {
+		this.isAbstract = isAbstract;
+	}
+
+	public String getPid() {
+		return pid;
+	}
+
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+}
